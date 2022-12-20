aa94720b876c413429cd9b2abf4f2f82a88058d5
blessey15 <blesseymariasaju@gmail.com>
2022-12-20 17:12:19 +0530

    set the index.js

--- fog-server/index.js
+++ fog-server/index.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const express = require('express');
+var bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+    console.log('Server is running on port 3000');
+});
+
+app.get('/',function(req,res){
+    res.send("Server is up and listening");
+});
+
+

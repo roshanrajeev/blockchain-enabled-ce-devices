441d8f412fe00287f829809e241b4fcc423fea25
blessey15 <blesseymariasaju@gmail.com>
2023-02-17 12:07:46 +0530

    added endpoint getsensordata

--- fog-server/index.js
+++ fog-server/index.js
@@ -1,7 +1,48 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const cors = require('cors');
+const { response } = require('express');
+
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/getsensordata', async (req, res) => {
+    const body = req.body;
+    const url1 = `http://localhost:3002/mine`;
+    const url2 = `http://localhost:3002/update`;
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    };
+    // to endpoint /mine
+    try {
+        const response = await fetch(url1, options);
+        const data = await response.json();
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send();
+    }
+    // to endpoint /update
+    try {
+        const response = await fetch(url2, options);
+        const data = await response.json();
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send();
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

442dfda708ce48d60432a6115db54c53e0665082
blessey15 <blesseymariasaju@gmail.com>
2023-04-16 12:07:38 +0530

    async not working

--- fog-server/index.js
+++ fog-server/index.js
@@ -24,43 +24,50 @@ const p2pserver = new P2pServer()
 p2pserver.listen()
 
 const associations = {'ldr':['light_bulb'],'camera':['light_bulb','tv']}
-
 // app.get("/heyy", (req, res) => {res.statusCode(200)})
 
-app.post("/heyy", (req, res) => {
-    axios({
-        method: "post",
-        url: `http://localhost:${String(HTTP_PORT_CHAIN).trim()}/mine`,
-        data: JSON.stringify(req.body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(() => console.log("data added to blockchain"))
+app.post("/heyy", async (req, res) => {
+    // axios({
+    //     method: "post",
+    //     url: `http://localhost:${String(HTTP_PORT_CHAIN).trim()}/mine`,
+    //     data: JSON.stringify(req.body),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    // .then(() => console.log("data added to blockchain"))
     const data = req.body.data
     const type = req.body.type
-    const index = Object.keys(associations).findIndex((association)=> (association===type))
-    associations[index].forEach(t => {
-
-        var sql = "SELECT ip from iot WHERE type=t";
-        var params = [t]
-        var ip_list = ""
-        db.get(sql, params, (err, row) => {
+    const index = Object.keys(associations).find((association)=> (association===type))
+    // console.log(Object.keys(associations).findIndex(key=> key==='camera'))
+    let ip_list = []
+    for(const t of associations[index]){
+        var sql = "SELECT ip from iot WHERE type=?";
+        var params = [t]   
+        await db.get(sql, params, (err, row) => {
             if (err) {
-              res.status(400).json({"error":err.message});
+            //   res.status(400).json({"error":err.message});
+            console.log(err)
               return;
             }
-        ip_list = row.ip 
-        });   
-    }); 
-                         
-    p2pserver.broadcast(data)
-    const val = 4096 - (((data - 0)/(2500 - 0)) * (4096 - 0) + 0) 
-    axios({
-        method: "get",
-        url: `http://192.168.1.100/update/?data=${val}`
-    })
-    .then("data forwarded to reactor")
+            console.log(row.ip)
+        ip_list.push(row.ip) 
+        });      
+    }
+    // associations[index].forEach(t => {
+        
+    // }); 
+    console.log(ip_list)                  
+    // p2pserver.broadcast(data)
+    // const val = 4096 - (((data - 0)/(2500 - 0)) * (4096 - 0) + 0) 
+    // ip_list.forEach((ip_val)=>{
+    //     axios({
+    //         method: "get",
+    //         url: `http://${ip_val}/update/?data=${val}`
+    //     })
+    //     .then(()=>console.log(`data forwarded to ip ${ip_val}`))
+    //     .catch((err)=>console.log(err))
+    // })
     res.sendStatus(200);
 })
 
@@ -95,7 +102,7 @@ app.post("/login", (req, res, next) => {
       });
     
     if(f==0){
-        var sql2 = 'INSERT INTO iot (iotid) VALUES (?)'
+        var sql2 = 'INSERT INTO iot (iotid,type,ip) VALUES (?,?,?)'
         var params2 = [req.body.iotid,req.body.type,req.body.ip]
         db.run(sql2, params2, function (err, result) {
             if (err){
@@ -109,20 +116,20 @@ app.post("/login", (req, res, next) => {
     }
 })
 
-// app.get("/data", (req, res, next) => {
-//     var sql = "select * from iot"
-//     var params = []
-//     db.all(sql, params, (err, rows) => {
-//         if (err) {
-//           res.status(400).json({"error":err.message});
-//           return;
-//         }
-//         res.json({
-//             "message":"success",
-//             "data":rows
-//         })
-//     });
-// });
+app.get("/data", (req, res, next) => {
+    var sql = "select * from iot"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
 
 app.listen(HTTP_PORT_FOG, () => {
     console.log('Server is running on port ' + HTTP_PORT_FOG)

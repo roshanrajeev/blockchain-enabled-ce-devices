ddfc45c8697599915992b415671a1321befb26b6
roshanrajeev <roshanrajeev2001@gmail.com>
2023-04-25 07:32:09 +0530

    Blockchain Config

--- blockchain/app/index.js
+++ blockchain/app/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Blockchain = require('../chain')
 const bodyParser = require('body-parser')
+
+require('dotenv').config()
+
 const P2pServer = require('./p2p-server')
 const Kwm = require('../Kwm')
 //get the port from the user or set the default port
